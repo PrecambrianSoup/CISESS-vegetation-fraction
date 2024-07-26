Update aggregation.py to fix aggregated lon calculation error

aggregation.py
@@ -23,7 +23,6 @@ def read_readme(fn, doy):
                 print('DOY(%03d) Out of Range: %d-%d' % (doy, int(min_doy), int(max_doy)))
                 return 1
         else:
-            print("DOY information not found in the text.")
             return 0
 
 # setting path for the directory with NetCDF and README files
@@ -33,18 +32,17 @@ input_path = '/Users/katieyang/Downloads/LP05'
 outdata = []
 output_path = os.path.join(input_path, 'FCOVER_Aggregation_Results.csv')
 
-# sets constants for the number of grid points and degrees of grid spacing
-nx = 43200*2        
+# sets constants for the number of grid cells around earth and the distance covered by each grid cell
+nx = 43200*2
 dx = 360.0/nx
-dx_grid = 360/43200.0/2
-        
+
 for fn in os.listdir(input_path):
     # process the file if it is a GBOV netcdf file and is native resolution
     if ('GBOV' in fn) and ('300M' not in fn) and fn.endswith(".nc"):
         print(fn)
         fname = os.path.join(input_path, fn)
 
-        # Some files contain extra characters, so create the read me file name accordingly
+        # Some native res files contain "20M" while others don't, so set the read me file name accordingly
         before, match, after = fn.partition('_20M')
         if match:
             fname2 = os.path.join(input_path, before+'_README.TXT')
@@ -76,10 +74,11 @@ for fn in os.listdir(input_path):
         qfout = dataset.variables['QF_OUT'][:,:]
         cl = dataset.variables['CL_95'][:,:]
         
-        # Converting latitude and longitude into grid indices and determines the number of unique grid cells
+        # Converting latitude and longitude into VNP09 grid indices
         iy = np.round((90-dx/2-lat)/dx)
         ix = np.round((lon*np.cos(lat*np.pi/180.0)-dx/2+180)/dx)
 
+        # Determines the number of unique grid cells
         idx = ix + iy*nx
         
         idx_unq = np.unique(idx)
@@ -89,24 +88,25 @@ for fn in os.listdir(input_path):
         mask = (qfin == 0) & (qfout == 0) & (fc >= 0)
         
         for i in range(0,n):
-            # Find the number of pixels that have the same pixel index and filters to find the number of valid pixels
+            # Find the number of pixels that have the same index for each unique grid index and filters to find the number of valid pixels
             tmp1 = idx == idx_unq[i]
             pixels = np.count_nonzero(tmp1)
             tmp2 = (idx == idx_unq[i]) & mask
             valid_pixels = np.count_nonzero(tmp2)
 
-            # Calculates the percentage of valid pixels and aggregates if over half are valid
+            # Calculates the percentage of valid pixels and aggregates if over 50% are valid and there are over 100 of them
             percent_agg = valid_pixels*100.0/pixels
 
-            if percent_agg > 50:
-                # Calculates the mean, standard deviation, and center longitude and latitude of the grid cell
+            if percent_agg > 50 and valid_pixels > 100:
+                # Calculates the mean fcover, standard deviation, and center longitude/latitude of the grid cell
                 agg_fc = np.mean(fc[tmp2])
                 std_fc = np.std(fc[tmp2])
-                lon = (idx_unq[i] % nx)*dx + dx/2 - 180
-                lat = 90-dx/2 - (idx_unq[i]/nx)*dx
-                
-                outdata.append([date, sat, site, lat, lon, agg_fc, std_fc, pixels, percent_agg])
+                lat_centered = (90-dx/2 - (idx_unq[i]/nx)*dx)
+                lon_centered = (((idx_unq[i] % nx)*dx + dx/2 - 180)/np.cos(lat_centered*np.pi/180.0))
+                outdata.append([date, sat, site, lat_centered, lon_centered, agg_fc, std_fc, pixels, percent_agg])
 
 df = pd.DataFrame(outdata, columns = ['Date','Sat','Site','Lat','Lon','FC500m','FCSTD','AggNum','AggPcnt'])
 
 df.to_csv(output_path, index=False, date_format='%Y-%m-%d')
+
+print(f"Aggregated data has been saved to {output_path}")