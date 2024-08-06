35c63cf99b3aec9100465d0c9578013ae99727e6
Katie Yang <113875818+PrecambrianSoup@users.noreply.github.com>
2024-08-05 20:54:50 -0400

    Update data_prep.py to exclude null reflectance/geometry data

--- data_prep.py
+++ data_prep.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 # Define the input and output file paths
 input_csv_path = '/Users/katieyang/Downloads/reflectance_and_geometry.csv'
@@ -8,8 +7,10 @@ output_csv_path = '/Users/katieyang/Downloads/LP05/FCOVER_Filtered_Results.csv'
 # Read the aggregated results CSV into a pandas DataFrame
 df = pd.read_csv(input_csv_path)
 
-# Filtering by cloud (q1 bit 2-3), shadow (q2 bit 3), and snow (q7 bit 0) flags
+# Drop rows where I1, I2, or I3 is null
+df = df.dropna(subset=['I1', 'I2', 'I3'])
 
+# Filtering by cloud (q1 bit 2-3), shadow (q2 bit 3), and snow (q7 bit 0) flags
 def confident_clear(qf1):
     if pd.isna(qf1):
         return False
@@ -30,7 +31,7 @@ def no_snow_present(qf7):
 
 # Apply the qf filters to the DataFrame and filter by aggregation amount/percentage
 filtered_df = df[df['QF1'].apply(confident_clear) & df['QF2'].apply(confident_clear) & df['QF7'].apply(no_snow_present)
-                 & (df['AggNum'] > 50) & (df['AggNum'] > 60)]
+                 & (df['AggNum'] > 350) & (df['AggPcnt'] > 97)]
 
 # Divide the I1, I2, I3 columns by 1000 and the azimuth/zenith columns by 100 for scaling
 df['I1'] = df['I1'] / 1000
@@ -44,4 +45,4 @@ df['SolarZenith'] = df['SolarZenith'] / 100
 # Save the filtered results into a new CSV file
 filtered_df.to_csv(output_csv_path, index=False)
 
-print(f"Filtering complete")
+print(f"Filtered data has been saved to {output_csv_path}")
